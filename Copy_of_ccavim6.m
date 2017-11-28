cdba2c3e50708e184524359c82292c3e347a1f62
victorgonav <32339257+voctorgonav@users.noreply.github.com>
2017-11-28 19:51:18 +0100

    -

--- Copy_of_ccavim6.m
+++ Copy_of_ccavim6.m
@@ -1,7 +1,7 @@
 close all;
 
 %% Leer imagen
-fileIDD1 = fopen('pam256.lum');
+fileIDD1 = fopen('camman.lum');
 A = fread(fileIDD1);
 B= zeros(256, 256);
 
@@ -30,20 +30,28 @@ Qinter = [8 16 19 22 26 27 29 34;
           26 27 29 34 38 46 56 69;
           27 29 35 38 46 56 69 83];
 
-%Q = Q/4;
+disp("---------------------------------------------------");
+disp("Las tablas de cuantificación se cuantifican por 1");
+%Qintra = Qintra/4;
+%Qinter = Qintra/4;
 
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  C O D E R
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Inicializacion
 dmin = 500000;
+disp("El área de búsqueda es de 50");
 alpha = 50;
 image = B;
 image2 = B;
 entra = 0;
 nun = 0;
+disp("El factor de comparador de variancias es 20");
+disp("---------------------------------------------------");
+
 
 %% Busqueda exhaustiva
 for inix=1:8:256-7
@@ -79,23 +87,18 @@ for inix=1:8:256-7
             image2(inix:inix+7,iniy:iniy+7) = ((image(posxmin:(posxmin+7),posymin:(posymin+7)))-(image(inix:(inix+7),iniy:(iniy+7))));
             Xinter = image2(inix:inix+7,iniy:iniy+7);
             Xintra = image(inix:inix+7,iniy:iniy+7);
-            if var(Xintra(:))<(var(Xinter(:))+20)
+
+            if var(Xintra(:))<(var(Xinter(:))+40)
                 Auxi = dct2(image(inix:inix+7,iniy:iniy+7));
                 Auxi = round(Auxi./Qintra); 
-                Auxi = Qintra.*Auxi;
                 l = 1;
             else
                 Auxi = dct2(image2(inix:inix+7,iniy:iniy+7));
                 Auxi = round(Auxi./Qinter); 
-                Auxi = Qinter.*Auxi;
                 l = 2;
             end
 
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
             image2(inix:inix+7,iniy:iniy+7) = (Auxi);
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
             matrix(nun,:) = [posxmin, posymin];
             mati(nun) = l;
@@ -110,12 +113,9 @@ end
 
 
 
-%%
-%figure(7);
-%imshow(uint8(image3))
 
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  D E C O D E R
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -127,9 +127,9 @@ for inix=1:8:256-7
         nun = nun +1;
         if matrix(nun,1) ~= 999
             if mati(nun) == 1
-                imagedecod(inix:inix+7,iniy:iniy+7) = idct2(image2(inix:inix+7,iniy:iniy+7));  
+                imagedecod(inix:inix+7,iniy:iniy+7) = idct2(Qintra.*image2(inix:inix+7,iniy:iniy+7));  
             else
-                imagex(inix:inix+7,iniy:iniy+7)=idct2(image2(inix:inix+7,iniy:iniy+7));
+                imagex(inix:inix+7,iniy:iniy+7)=idct2(Qinter.*image2(inix:inix+7,iniy:iniy+7));
                 imagedecod(inix:inix+7,iniy:iniy+7) = (imagedecod(matrix(nun,1):matrix(nun,1)+7,matrix(nun,2):matrix(nun,2)+7)-(imagex(inix:inix+7,iniy:iniy+7)));   
             end
         else
